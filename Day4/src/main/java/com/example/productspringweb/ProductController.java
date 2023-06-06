cda11e199fe078edbb84777aa3f4b77b83f4a4cb
Punit Sakre <punitsakre786@gmail.com>
2023-06-06 17:40:33 +0530

    built product management application using spring mvc

---
+++ Day4/src/main/java/com/example/productspringweb/ProductController.java
@@ -0,0 +1,29 @@
+package com.example.productspringweb;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class ProductController {
+
+    @Autowired
+    ProductServiceUsingStream service;
+
+    @GetMapping("/products")
+    public List<Product> getAllProducts() {
+        return service.getAllProducts();
+    }
+
+    @GetMapping("/product/{name}")
+    public Product getProduct(@PathVariable String name) {
+        return service.getProduct(name);
+    }
+
+    @PostMapping("/product")
+    public void addProduct(@RequestBody Product p) {
+        service.addProduct(p);
+    }
+
+}
---
+++ Day4/src/main/java/com/example/productspringweb/ProductDB.java
@@ -0,0 +1,18 @@
+package com.example.productspringweb;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductDB extends JpaRepository<Product, Integer> {
+
+    Product findProductByName(String name);
+
+    List<Product> findProductsByPlaceContainsIgnoreCase(String place);
+
+    List<Product> findProductByNameContainsIgnoreCaseOrTypeContainsIgnoreCaseOrPlaceContainsIgnoreCase(String text1, String text2, String text3);
+
+    List<Product> getProductByWarrantyIsLessThan(int year);
+}
